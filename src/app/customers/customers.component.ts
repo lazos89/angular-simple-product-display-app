9aeedabd9e7d6ae97b47f795bb99d31b0082eed6
Lazos Doug <lazarosd89@gmail.com>
2020-04-15 13:56:01 +0300

    customers convert to reactive form

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Customer } from "./customer";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-customers',
-  templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.scss']
+  selector: "app-customer",
+  templateUrl: "./customers.component.html",
+  styleUrls: ["./customers.component.scss"],
 })
 export class CustomersComponent implements OnInit {
+  customer = new Customer();
+  customerForm: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: "",
+      lastName: "",
+      email: "",
+      sendCatalog: true,
+    });
+    console.log(this.customerForm);
   }
 
+  save() {
+    console.log(this.customerForm);
+    console.log("Saved: " + JSON.stringify(this.customerForm));
+  }
+  populateTestData() {
+    // this.customerForm.setValue({
+    //   firstName: "Lazos",
+    //   lastName: "Doug",
+    //   email: "lazos@test.com",
+    //   sendCatalog: true,
+    // });
+    this.customerForm.patchValue({
+      firstName: "Lazos",
+      lastName: "Doug",
+      sendCatalog: true,
+    });
+  }
 }
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CustomersComponent } from "./customers.component";
 import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [CustomersComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
     RouterModule.forChild([{ path: "", component: CustomersComponent }]),
   ],
 })
